1b62740ecfddfbe1fb58d07d7694e9fea8d384f6
Marcial Ambriz <jmaramgo@gmail.com>
2020-05-06 14:29:40 -0500

    Connetion to server beteewn customers

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
+const http = require('http');
 const express = require('express');
 const app = express();
+const io = require('socket.io');
 
-app.get('/', (req, res) => {
-  res.end("Chat Iniciado");
-});
+const server = http.createServer(app);
+// app.get('/', (req, res) => {
+//   res.end("Chat Iniciado");
+// });
 
-app.listen(3000, () => {
+app.set('port', 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+// console.log(__dirname);
+
+server.listen(app.get('port'), () => {
   console.log('Servidor Iniciado');
 });
+
+var sockets = io.listen(server);
+
+sockets.on('connection', (socket) => {
+  console.log('Nuevo Cliente');
+  socket.on('message-client', (data) => {
+    console.log(data);
+    sockets.emit('message-server', data);
+  });
+});
+
--- public/main.js
+++ public/main.js
@@ -1,13 +1,28 @@
 $(()=>{
+  // socket-iniciado
+  var socket = io();
 
   console.log('Cargado!');
   // let en = 1
   const message = $('#chat-message');
+  const chat = $('#chat');
+
+  message.focus();
+
   // -------------------------------------------button-effect
   $('#message-box').submit((e) => {
     e.preventDefault();
     // console.log('Enviado'+en);
-    console.log(message.val());
+    // console.log(message.val());
+    // chat.append(message.val() + "<br>");
+    socket.emit('message-client', message.val());
+    message.val('');
     // en++;
   });
+
+  socket.on('message-server', (data) => {
+    console.log(data);
+    chat.append(data + '<br>');
+  });
+
 });
